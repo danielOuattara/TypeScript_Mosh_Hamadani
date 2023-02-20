cbc9cfb26a6e6d00eb9179579864700505be9107
danielOuattara <daniel.ouattara@gmx.com>
2023-02-20 21:10:58 +0100

    arrow func

---
+++ course-5-arrow-function/index.js
@@ -0,0 +1,7 @@
+var log = function (message) {
+    console.log(message);
+};
+var log2 = function (message) { return console.log(message); };
+var log3 = function (message) { return console.log(message); };
+var log4 = function () { return console.log("Hello"); };
+// ----------------------------------------------
---
+++ course-5-arrow-function/index.ts
@@ -0,0 +1,10 @@
+let log = function (message) {
+  console.log(message);
+};
+
+let log2 = (message) => console.log(message);
+let log3 = (message: any) => console.log(message);
+
+let log4 = () => console.log("Hello");
+
+// ----------------------------------------------
